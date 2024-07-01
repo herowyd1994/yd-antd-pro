576fcc5a420cf29509a9c0f7fc26f59bd6cef3da
王亚冬 <724743070@qq.com>
2024-07-01 09:45:34 +0800

    feature: useModalTable

--- useModalTable/index.ts
+++ useModalTable/index.ts
@@ -4,14 +4,18 @@ import { useTable } from '../index';
 import { useStore } from '@yd/r-hooks';
 import { Props, Store } from './types';
 
-export default <D extends Record<string, any>>({ title, width, ...props }: Props) => {
-    const { tableProps } = useTable<D>(props);
+export default <D extends Record<string, any>>({ title, width, done, ...props }: Props) => {
+    const { tableProps, data, params, total, ...table } = useTable<D>(props);
     const { visible, dispatch } = useStore<Store>({ visible: false });
     const onShow = async () => {
         dispatch({ visible: true });
         tableProps.actionRef.current?.reloadAndRest();
     };
     const onClose = () => dispatch({ visible: false });
+    const onConfirm = async () => {
+        await done?.(params, data, total);
+        dispatch({ visible: false });
+    };
     return {
         modalProps: {
             title,
@@ -23,6 +27,8 @@ export default <D extends Record<string, any>>({ title, width, ...props }: Props
             ...tableProps,
             manualRequest: true
         },
-        onShow
+        onShow,
+        onConfirm,
+        ...table
     };
 };
--- useModalTable/types.ts
+++ useModalTable/types.ts
@@ -5,6 +5,7 @@ import { Props as TableProps } from '../useTable/types';
 export interface Props<D = any> extends TableProps<D> {
     title?: string;
     width?: number;
+    done?(params: Record<string, any>, data: D[], total: number): Promise<void> | void;
 }
 export interface Store {
     visible: boolean;

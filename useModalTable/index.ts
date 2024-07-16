9511de0d1b38f6c4349cf9d95edd7edfca8eba37
王亚冬 <724743070@qq.com>
2024-07-16 13:48:16 +0800

    fix: useModalTable

--- useModalTable/index.ts
+++ useModalTable/index.ts
@@ -3,12 +3,14 @@
 import { useTable } from '../index';
 import { useStore } from '@yd/r-hooks';
 import { Props, Store } from './types';
+import { sleep } from '@yd/utils';
 
 export default <D extends Record<string, any>>({ title, width, done, ...props }: Props) => {
     const { tableProps, data, params, total, ...table } = useTable<D>(props);
     const { visible, dispatch } = useStore<Store>({ visible: false });
     const onShow = async () => {
         dispatch({ visible: true });
+        await sleep(150);
         tableProps.actionRef.current?.reloadAndRest?.();
     };
     const onClose = () => dispatch({ visible: false });
